e27e236896e52940a2273fc3c6ce0f68f66f9d25
ABHISHEK KUMAR <abhi22112004@gmail.com>
2026-08-02 15:49:44 +0530

    Time: 0 ms (100%), Space: 10.4 MB (16.32%) - LeetHub - Time: O(n^2), Space: O(n^2)

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
     int solve(vector<int>&nums,int left,int right){
-        // if(left>right) return 0;
+        int n = nums.size();
+        if(dp[left][right]!=-1){
+            return dp[left][right];
+        }
+        if(left>right) return 0;
         if(left==right) return nums[right];
         int pickleft = nums[left] - solve(nums,left+1,right);
         int pickright = nums[right] - solve(nums,left,right-1);
-        return max(pickleft,pickright);
+        dp[left][right] =  max(pickleft,pickright);
+        return dp[left][right];
     }
     bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        dp.assign(n,vector<int>(n,-1));
         return solve(nums,0,nums.size()-1)>=0;
     }
 };
