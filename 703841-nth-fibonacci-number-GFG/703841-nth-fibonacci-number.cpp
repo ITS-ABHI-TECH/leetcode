471e21c7ad25546c58a7a8b3168457fe3dd6654c
ABHISHEK KUMAR <abhi22112004@gmail.com>
2026-08-28 21:32:20 +0530

    Added solution - Time: O(2^n), Space: O(n)

---
+++ 703841-nth-fibonacci-number-GFG/703841-nth-fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int nthFibonacci(int n) {
+        // code here
+        if(n==1) return 1;
+        if(n==0) return 0;
+        return nthFibonacci(n-1)+nthFibonacci(n-2);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
