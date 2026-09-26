85ce48e8bd37b0affb520b7b0dcd4df12991dff9
ABHISHEK KUMAR <abhi22112004@gmail.com>
2026-09-26 20:29:52 +0530

    Time: 65 ms (90.34%), Space: 122.8 MB (46.73%) - LeetHub - Time: O(N + K), Space: O(K)

---
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        unordered_map<string, string> mp;
+
+        for (auto &x : knowledge) {
+            mp[x[0]] = x[1];
+        }
+
+        string ans;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != '(') {
+                ans += s[i];
+            } else {
+                string key = "";
+                i++;
+
+                while (s[i] != ')') {
+                    key += s[i];
+                    i++;
+                }
+
+                if (mp.count(key))
+                    ans += mp[key];
+                else
+                    ans += "?";
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
